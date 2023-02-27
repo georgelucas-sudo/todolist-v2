ac791fb5e9e3474315ad98b032530fbdfadb3497
Yiga George Lucas <georgedalucas111@gmail.com>
2023-02-27 20:32:33 +0300

    updated the keys

--- app.js
+++ app.js
@@ -182,7 +182,11 @@ app.post("/delete", function(req, res) {
 app.get("/about", function(req, res) {
     res.render("about");
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000
+}
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
     console.log("Server started on port 3000");
 });
